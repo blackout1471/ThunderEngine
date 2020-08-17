dc69c001affeffcecb026c6a140e953f315a081f
Emil Andersen <EADR@nnit.com>
2020-08-17 13:28:09 +0200

    Added some stuff to sandbox

--- Sandbox2D/Main.cpp
+++ Sandbox2D/Main.cpp
@@ -1,17 +1,23 @@
 #include "ThunderEngine.h"
+#include "Platform/RenderApi.h"
 
 using ThunderEngine::Application::Debug;
 
 int main(void)
 {
 
-    Application::GameWindow* gameWindow = new Application::GameWindow();
-    Debug debug;
-    debug.Write("hello ");
+    ThunderEngine::Application::GameWindow* gameWindow = new ThunderEngine::Application::GameWindow();
 
     gameWindow->CreateWindow(640,320,"Yes", NULL, NULL);
+    gameWindow->MakeContextCurrent();
+
+    ThunderEngine::RenderApi::CreateApi(ThunderEngine::RenderVendor::OpenGL);
+    ThunderEngine::RenderApi::GetApi()->Init();
+
     while (!gameWindow->ShouldWindowClose())
     {
+        ThunderEngine::RenderApi::GetApi()->ClearColor(.0, .0, .8, 1.0);
+        ThunderEngine::RenderApi::GetApi()->ClearColorBit();
         gameWindow->SwapBuffers();
         gameWindow->PollEvents();
     }
